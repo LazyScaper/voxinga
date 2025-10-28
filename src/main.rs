6196c7286d5d2ff9df53d7950b2cf0431cc61f00
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-10-28 21:27:53 +0000

    [Jake] Add FPS counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
+use bevy::prelude::*;
+use std::time::Duration;
+
+#[derive(Component)]
+struct FpsText;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Text::new("FPS: "),
+        TextFont {
+            font_size: 42.0,
+            ..default()
+        },
+        FpsText,
+    ));
+}
+
+fn fps_update_system(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
+    for mut text in &mut query {
+        let fps = diagnostics
+            .get(&FrameTimeDiagnosticsPlugin::FPS)
+            .and_then(|fps| fps.smoothed())
+            .unwrap_or(-1.0);
+
+        **text = format!("FPS {:.2}", fps)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((DefaultPlugins, FrameTimeDiagnosticsPlugin::default()))
+        .insert_resource(Time::<Fixed>::from_duration(Duration::from_millis(100)))
+        .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, fps_update_system)
+        .run();
 }
