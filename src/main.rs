eda50b012fa035f9ce5542ab8b184279183151c3
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-10-29 21:39:53 +0000

    [Jake] Spawn some starter cubes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,44 @@ use std::time::Duration;
 #[derive(Component)]
 struct FpsText;
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    setup_gui(&mut commands);
+
+    // cube
+    for i in 1..10 {
+        for j in 1..10 {
+            commands.spawn((
+                Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+                MeshMaterial3d(materials.add(if (i + j) % 2 == 0 {
+                    Color::BLACK
+                } else {
+                    Color::WHITE
+                })),
+                Transform::from_xyz(i as f32, 0.0, j as f32),
+            ));
+        }
+    }
+
+    // light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
+
+fn setup_gui(commands: &mut Commands) {
     commands.spawn(Camera2d);
 
     commands.spawn((
