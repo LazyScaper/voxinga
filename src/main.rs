d9f2a0cd233db3006707a6093d1fad385136ed84
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-11-01 17:14:51 +0000

    [Jake] Add a player controller and use avian

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use avian3d::prelude::{AngularVelocity, Collider, LockedAxes, PhysicsDebugPlugin, RigidBody};
+use avian3d::PhysicsPlugins;
 use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy::window::PresentMode;
+use bevy_tnua::prelude::*;
+use bevy_tnua_avian3d::prelude::*;
+use bevy_tnua_avian3d::TnuaAvian3dPlugin;
 use std::time::Duration;
+use bevy::color::palettes::css;
 
 #[derive(Component)]
 struct FpsText;
@@ -13,20 +19,17 @@ fn setup(
 ) {
     setup_gui(&mut commands);
 
-    // cube
-    for i in 1..10 {
-        for j in 1..10 {
-            commands.spawn((
-                Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-                MeshMaterial3d(materials.add(if (i + j) % 2 == 0 {
-                    Color::BLACK
-                } else {
-                    Color::WHITE
-                })),
-                Transform::from_xyz(i as f32, 0.0, j as f32),
-            ));
-        }
-    }
+    setup_grid(&mut commands, &mut meshes, &mut materials);
+
+    // Dynamic physics object with a collision shape and initial angular velocity
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::cuboid(1.0, 1.0, 1.0),
+        AngularVelocity(Vec3::new(2.5, 10.5, 1.5)),
+        Mesh3d(meshes.add(Cuboid::from_length(1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(2.0, 4.0, 2.0),
+    ));
 
     // light
     commands.spawn((
@@ -42,6 +45,31 @@ fn setup(
         Transform::from_translation(Vec3::new(15.0, 10.0, 15.0)).looking_at(Vec3::ZERO, Vec3::Y),
         Camera3d::default(),
     ));
+}
+
+fn setup_grid(
+    commands: &mut Commands,
+    mut meshes: &mut ResMut<Assets<Mesh>>,
+    mut materials: &mut ResMut<Assets<StandardMaterial>>,
+) {
+    let grid_size = 10;
+    for i in 0..grid_size {
+        for j in 0..grid_size {
+            let color = if (i + j) % 2 == 0 {
+                Color::srgb(0.0, 0.0, 0.0) // Green
+            } else {
+                Color::srgb(255.0, 255.0, 255.0) // Red
+            };
+
+            commands.spawn((
+                RigidBody::Static,
+                Mesh3d(meshes.add(Cuboid::from_length(1f32))),
+                MeshMaterial3d(materials.add(color)),
+                Collider::cuboid(1.0, 1.0, 1.0),
+                Transform::from_xyz(i as f32, 0f32, j as f32),
+            ));
+        }
+    }
 }
 
 fn setup_gui(commands: &mut Commands) {
@@ -74,6 +102,78 @@ fn fps_update_system(
     }
 }
 
+fn setup_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Capsule3d {
+            radius: 0.5,
+            half_length: 0.5,
+        })),
+        MeshMaterial3d(materials.add(Color::from(css::DARK_CYAN))),
+        Transform::from_xyz(3.0, 2.0, 3.0),
+        // The player character needs to be configured as a dynamic rigid body of the physics
+        // engine.
+        RigidBody::Dynamic,
+        Collider::capsule(0.5, 1.0),
+        // This is Tnua's interface component.
+        TnuaController::default(),
+        // A sensor shape is not strictly necessary, but without it we'll get weird results.
+        TnuaAvian3dSensorShape(Collider::cylinder(0.49, 0.0)),
+        // Tnua can fix the rotation, but the character will still get rotated before it can do so.
+        // By locking the rotation we can prevent this.
+        LockedAxes::ROTATION_LOCKED,
+    ));
+}
+
+fn apply_controls(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<&mut TnuaController>) {
+    let Ok(mut controller) = query.single_mut() else {
+        return;
+    };
+
+    let mut direction = Vec3::ZERO;
+
+    if keyboard.pressed(KeyCode::ArrowUp) {
+        direction -= Vec3::Z;
+    }
+    if keyboard.pressed(KeyCode::ArrowDown) {
+        direction += Vec3::Z;
+    }
+    if keyboard.pressed(KeyCode::ArrowLeft) {
+        direction -= Vec3::X;
+    }
+    if keyboard.pressed(KeyCode::ArrowRight) {
+        direction += Vec3::X;
+    }
+
+    // Feed the basis every frame. Even if the player doesn't move - just use `desired_velocity:
+    // Vec3::ZERO`. `TnuaController` starts without a basis, which will make the character collider
+    // just fall.
+    controller.basis(TnuaBuiltinWalk {
+        // The `desired_velocity` determines how the character will move.
+        desired_velocity: direction.normalize_or_zero() * 1.1,
+        // The `float_height` must be greater (even if by little) from the distance between the
+        // character's center and the lowest point of its collider.
+        float_height: 1.5,
+        // `TnuaBuiltinWalk` has many other fields for customizing the movement - but they have
+        // sensible defaults. Refer to the `TnuaBuiltinWalk`'s documentation to learn what they do.
+        ..Default::default()
+    });
+
+    // Feed the jump action every frame as long as the player holds the jump button. If the player
+    // stops holding the jump button, simply stop feeding the action.
+    if keyboard.pressed(KeyCode::Space) {
+        controller.action(TnuaBuiltinJump {
+            // The height is the only mandatory field of the jump button.
+            height: 4.0,
+            // `TnuaBuiltinJump` also has customization fields with sensible defaults.
+            ..Default::default()
+        });
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins((
@@ -84,10 +184,19 @@ fn main() {
                 }),
                 ..default()
             }),
+            PhysicsPlugins::default(),
+            PhysicsDebugPlugin,
             FrameTimeDiagnosticsPlugin::default(),
+            TnuaControllerPlugin::new(FixedUpdate),
+            TnuaAvian3dPlugin::new(FixedUpdate),
         ))
-        .insert_resource(Time::<Fixed>::from_duration(Duration::from_millis(100)))
-        .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, fps_update_system)
+        .add_systems(Startup, (setup, setup_player))
+        .add_systems(
+            FixedUpdate,
+            (
+                fps_update_system,
+                apply_controls.in_set(TnuaUserControlsSystems),
+            ),
+        )
         .run();
 }
